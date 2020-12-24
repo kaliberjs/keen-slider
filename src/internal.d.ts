ecdc30aed240e0bf65c5d8903ed2f61dfb41f0e7
EECOLOR <eecolor@qirx.org>
2020-12-24 21:21:06 +0100

    Remove comma's in (temporary) type definition

--- src/internal.d.ts
+++ src/internal.d.ts
@@ -1,38 +1,38 @@
 import { TOptions } from '../index'
 
 export type BaseOptionType = {
-  isLoop: boolean,
-  isRubberband: boolean,
-  isVerticalSlider: boolean,
-  isRtl: boolean,
-  isCentered: boolean,
+  isLoop: boolean
+  isRubberband: boolean
+  isVerticalSlider: boolean
+  isRtl: boolean
+  isCentered: boolean
 }
 
 export type TranslatedOptionsType = BaseOptionType & {
-  enableDragControls: boolean,
-  touchMultiplicator(val: number): number,
-  cancelOnLeave: boolean,
-  initialIndex: number,
-  preventEventAttributeName: string,
-  duration: number,
-  friction: number,
-  dragEndMove: 'snap' | 'free-snap' | 'free',
-  slides: Array<HTMLElement> | null,
-  numberOfSlides: number,
-  widthOrHeight: number,
-  isIndexOutOfBounds(idx: number): boolean,
-  ensureIndexInBounds(idx: number): number,
-  strategy: StrategyType,
+  enableDragControls: boolean
+  touchMultiplicator(val: number): number
+  cancelOnLeave: boolean
+  initialIndex: number
+  preventEventAttributeName: string
+  duration: number
+  friction: number
+  dragEndMove: 'snap' | 'free-snap' | 'free'
+  slides: Array<HTMLElement> | null
+  numberOfSlides: number
+  widthOrHeight: number
+  isIndexOutOfBounds(idx: number): boolean
+  ensureIndexInBounds(idx: number): number
+  strategy: StrategyType
 }
 
 export type StrategyType = {
-  maxPosition: number,
-  trackLength: number,
+  maxPosition: number
+  trackLength: number
   calculateSlidePositions(progress: number): Array<SlidePositionType>
   calculateIndex(position: number): number
   calculateIndexTrend(position: number): number
   getDetails(): {
-    slidesPerView: number,
+    slidesPerView: number
   }
   getSizeStyle(): string
   getSlidePosition(idx: number, slidePosition: SlidePositionType )
@@ -40,6 +40,6 @@ export type StrategyType = {
 }
 
 type SlidePositionType = {
-  portion: number,
-  distance: number,
+  portion: number
+  distance: number
 }
